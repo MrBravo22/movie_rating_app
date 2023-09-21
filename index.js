8c373a199e2e516c9980c4e6200348844a5d86ff
Elijah Bravo <elijahbravo22@yahoo.com>
2023-09-21 17:40:04 -0600

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const pg = require('pg');
-const client = new pg.Client('postgres://localhost/fullstack_template_db');
+const client = new pg.Client('postgres://localhost/movie_rating_db');
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -16,12 +16,34 @@ app.get('/dist/main.js.map', (req, res)=> res.sendFile(reactSourceMap));
 const styleSheet = path.join(__dirname, 'styles.css');
 app.get('/styles.css', (req, res)=> res.sendFile(styleSheet));
 
+app.get('/api/movies', async (req, res, next) => {
+  try {
+    const SQL = `
+    SELECT *
+    FROM movies
+    `;
+    const response = await client.query(SQL);
+    res.send(response.rows);
+  }
+  catch(ex){
+    next(ex)
+  }
+});
+
 const init = async()=> {
   await client.connect();
   console.log('connected to database');
   const SQL = `
-    SQL SETUP AND SEED
+    DROP TABLE IF EXISTS movies;
+    CREATE TABLE movies(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(100)
+    );
+    INSERT INTO movies(name) VALUES('Dumb and Dumber');
+    INSERT INTO movies(name) VALUES('All Quiet on the Western Front');
+    INSERT INTO movies(name) VALUES('Sharknado');
   `;
+  await client.query(SQL);
   console.log('create your tables and seed data');
 
   const port = process.env.PORT || 3000;
